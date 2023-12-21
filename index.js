2033d27598577a5242da452ec894f066a9c4f9ba
codingInEME <107263899+codingInEME@users.noreply.github.com>
2023-12-21 21:52:03 +0500

    Add reset button
    - Add current size indicator

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 const container = document.querySelector(".container");
+const grid_size_indicator = document.querySelector("#grid-size");
+const reset_button = document.querySelector("#reset-button");
 const grid_border = document.createElement("div");
+
 const MIN_SIZE = 2;
 const MAX_SIZE = 100;
 const DEFAULT_SIZE = 16;
 
+
 grid_border.className = "grid-border";
 container.appendChild(grid_border);
 
-let grid_size = inputGridSize();
+createGrid(DEFAULT_SIZE);
 
-for (let i = 0; i < grid_size; i++) {
-    const row = document.createElement("div");
-    row.className = "tileRow"
+reset_button.addEventListener("click", () => {
+    let grid_size = inputGridSize();
+    createGrid(grid_size);
+});
 
-    for (let j = 0; j < grid_size; j++) {
-        const tile = document.createElement("div");
-        tile.className = "tile";
-        tile.addEventListener("mouseenter", colorTile);
-        row.appendChild(tile);
+function createGrid(grid_size) {
+    let rows = [];
+
+    grid_size_indicator.textContent = `Grid Size: ${grid_size}`;
+    for (let i = 0; i < grid_size; i++) {
+        const row = document.createElement("div");
+        row.className = "tileRow"
+
+        for (let j = 0; j < grid_size; j++) {
+            const tile = document.createElement("div");
+            tile.className = "tile";
+            tile.addEventListener("mouseenter", colorTile);
+            row.appendChild(tile);
+        }
+
+        rows.push(row);
     }
 
-    grid_border.appendChild(row);
+    grid_border.replaceChildren(...rows);
 }
 
 function inputGridSize() {
